53d3d1fee41fe231e14fee7d47930a594ff0487b
Im Yongbeen <meis1541@naver.com>
2019-09-13 17:01:55 +0900

    [B] Test Graphql

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,11 @@
-console.log('bood')
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
+
+const server = new GraphQLServer({
+  typeDefs: "graphql/schema.graphql",
+  resolvers
+});
+
+server.start(() => {
+  console.log("Graphql Server Running");
+});
