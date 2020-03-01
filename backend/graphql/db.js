efdbd6e9b163b93c2e94c4c549381a99b70129f4
jungpaeng <meis1541@naver.com>
2020-03-01 16:15:55 +0900

    [B] Extend the Schema

---
+++ backend/graphql/db.js
@@ -0,0 +1,20 @@
+export const people = [
+  {
+    id: "0",
+    name: 'Person A',
+    age: 18,
+    gender: 'male'
+  },
+  {
+    id: "1",
+    name: 'Person B',
+    age: 18,
+    gender: 'female'
+  },
+];
+
+export const getById = id => {
+  const filteredPeople = people.filter(person => person.id === id);
+
+  return filteredPeople[0];
+};
--- backend/graphql/resolver.js
+++ backend/graphql/resolver.js
@@ -1,6 +1,8 @@
+import {people} from "./db";
+
 const resolvers = {
   Query: {
-    name: () => "yongbeen"
+    people: () => people,
   }
 };
 
