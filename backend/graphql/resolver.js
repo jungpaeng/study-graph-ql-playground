9292de9d29b60a6aaba0f6a30160c622582f1503
jungpaeng <meis1541@naver.com>
2020-03-01 15:03:02 +0900

    [B] Create the test Query and Resolver

---
+++ backend/graphql/resolver.js
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    name: () => "yongbeen"
+  }
+};
+
+export default resolvers;
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-console.log('nodemon');
+import {GraphQLServer} from 'graphql-yoga';
+import resolvers from "./graphql/resolver";
+
+const server = new GraphQLServer({
+  typeDefs: "graphql/schema.graphql",
+  resolvers,
+});
+
+server.start(() => console.log('GraphQL Server Running'));
