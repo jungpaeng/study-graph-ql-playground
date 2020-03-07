9e455ab84f4d477b666178d32e80df9c22899957
jungpaeng <meis1541@naver.com>
2020-03-07 22:10:51 +0900

    [F] GET MOVIES Query

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,1 +1,24 @@
-export default () => "Home";
+import * as React from 'react';
+import {gql} from 'apollo-boost';
+import {useQuery} from '@apollo/react-hooks';
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Home = () => {
+  const {loading, error, data} = useQuery(GET_MOVIES);
+
+  if (loading) {
+    return "loading..."
+  } else if (data && data.movies) {
+    return data.movies.map(item => <h1>{item.id}</h1>)
+  }
+};
+
+export default Home;
