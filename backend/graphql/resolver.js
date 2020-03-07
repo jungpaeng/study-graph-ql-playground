c3cc53656a2d0126e000cff64a28d3c7359231b2
jungpaeng <meis1541@naver.com>
2020-03-07 21:20:08 +0900

    [B] Apply axios, Create getMovie Query

--- backend/graphql/resolver.js
+++ backend/graphql/resolver.js
@@ -1,7 +1,8 @@
-import {getMovies} from "./db";
+import {getMovie, getMovies} from "./db";
 
 const resolvers = {
   Query: {
+    movie: (_, {id}) => getMovie(id),
     movies: (_, {limit, rating}) => getMovies(limit, rating),
   },
 };
